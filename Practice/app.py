67f8b0190b4b7dd7dcb6ce18a77a244ae77ab7d5
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-03-24 13:18:26 +0530

    flask practice

--- Practice/app.py
+++ Practice/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLALchemy
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+db = SQLALchemy(app)
+ 
 @app.route('/')
 def hello_world():
    return render_template('index.html')
