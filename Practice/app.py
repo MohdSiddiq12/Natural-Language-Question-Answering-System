cfdcad018d665d808ebab4323a8361a16b50bb9b
Mohammed Siddiq <97431769+MohdSiddiq12@users.noreply.github.com>
2024-03-24 15:56:26 +0530

    flask practice

--- Practice/app.py
+++ Practice/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLALchemy
+from datetime import datetime
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
 db = SQLALchemy(app)
+
+class Todo(db.model):
+   id = db.column(db.integer, primary_key = True)
+   content = db.column(db.string(200), nullable=False)
+   completed = db.column(db.integer, default=0)
+   db.created = db.column(db.DateTime, default= datetime.utcnow)
+
+   def __repr__(self):
+      return '<Task %r>' % self.id
+
  
 @app.route('/')
 def hello_world():
